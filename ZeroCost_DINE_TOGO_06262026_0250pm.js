99e2abd805b74a6d42c998a7824316cd34c56d4c
tomasmorato-northpark <tomasmorato.northpark@gmail.com>
2026-06-26 18:08:24 +0800

    Update ZeroCost_DINE_TOGO_06262026_0250pm.js

--- ZeroCost_DINE_TOGO_06262026_0250pm.js
+++ ZeroCost_DINE_TOGO_06262026_0250pm.js
@@ -1,4 +1,20 @@
-{
-    "ZC04 CALAMANSI": 5598
-    
-}  
+(async () => {
+    const pos = odoo.__WOWL_DEBUG__.root.env.services.pos;
+
+    const rules = {
+        "ND71 SWEET & SOUR PORK": 5603
+       
+    };
+
+    for (const line of pos.selectedOrder.get_orderlines()) {
+        const container = rules[line.product_id.display_name];
+
+        if (!container) continue;
+
+        for (let i = 0; i < line.qty; i++) {
+            await pos.addLineToCurrentOrder({
+                product_id: pos.models["product.product"].get(container)
+            });
+        }
+    }
+})();
